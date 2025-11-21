be8a5b8fa76c1b7bc3ca20b51ef5db321f0721db
Kyle Nazario <nazariok@moodys.com>
2025-11-21 11:07:21 -0700

    Sets autoread

--- init.lua
+++ init.lua
@@ -37,3 +37,10 @@ require('nvim-autopairs').setup()
 vim.cmd("source ~/.vimrc")
 vim.opt.wrap = false
 vim.g.loaded_matchparen = true
+
+-- Enable autoread and set up checking triggers
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = "*",
+})
